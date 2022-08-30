9c90b6a21b380c7cec0042586ffb130372a99141
luchunchen <ji3g48xj6@gmail.com>
2022-08-30 23:36:45 +0800

    第一步商品功能

---
+++ src/main/java/com/example/springbootmall/serveice/ProductService.java
@@ -0,0 +1,7 @@
+package com.example.springbootmall.serveice;
+
+import com.example.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer productId);
+}
