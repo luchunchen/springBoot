8d6d01def9e56e28b5b9a9d0c4823b6b4c4d9a6b
luchunchen <ji3g48xj6@gmail.com>
2022-09-03 23:31:21 +0800

    商品功能- 新增基本查詢商品功能

--- src/main/java/com/example/springbootmall/dao/ProductDao.java
+++ src/main/java/com/example/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.example.springbootmall.dao;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/example/springbootmall/serveice/ProductService.java
+++ src/main/java/com/example/springbootmall/serveice/ProductService.java
@@ -3,7 +3,11 @@ package com.example.springbootmall.serveice;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
