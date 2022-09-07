31477f3653f476a87fce7ea96ecd36a527fdb784
luchunchen <ji3g48xj6@gmail.com>
2022-09-07 23:51:50 +0800

    帳號功能- 註冊新帳號

---
+++ src/main/java/com/example/springbootmall/serveice/UserService.java
@@ -0,0 +1,10 @@
+package com.example.springbootmall.serveice;
+
+import com.example.springbootmall.dto.UserRegisterRequest;
+import com.example.springbootmall.model.User;
+
+public interface UserService {
+
+    User getUserById(Integer userId);
+    Integer register(UserRegisterRequest userRegisterRequest);
+}
